a560a736d1c5c29b5c9461d66da21d00c03ee92e
Pascal Bellard <pascal.bellard@slitaz.org>
2012-06-01 17:14:10 +0200

    Move to wok screenlets python-werkzeug plymouth network-manager n2nc kompozer hiawatha awstats-hotspot

--- awstats-hotspot/stuff/loghotspot.sh
+++ awstats-hotspot/stuff/loghotspot.sh
@@ -4,30 +4,32 @@ HOTSPOT="hotspot"
 DIRDATA="/var/lib/awstats/$HOTSPOT"
 
 if [ "$1" == "--install" ]; then
-	sed -e "s,^LogFile=.*,LogFile=\"$0 < /var/log/squid/access.log |\"," \
-	    -e 's/^LogFormat=.*/LogFormat="%code %time4 %bytesd %method %query %host %url"/' \
-	    -e "s/^SiteDomain=.*/SiteDomain=\"$HOTSPOT\"/" \
-	    -e "s,^DirData=.*,DirData=\"$DIRDATA\"," \
-	    -e 's/^LevelForBrowsersDetection=.*/LevelForBrowsersDetection=0/' \
-	    -e 's/^LevelForOSDetection=.*/LevelForOSDetection=0/' \
-	    -e 's/^LevelForRefererAnalyze=.*/LevelForRefererAnalyze=0/' \
-	    -e 's/^LevelForRobotsDetection=.*/LevelForRobotsDetection=0/' \
-	    -e 's/^LevelForSearchEnginesDetection=.*/LevelForSearchEnginesDetection=0/' \
-	    -e 's/^LevelForKeywordsDetection=.*/LevelForKeywordsDetection=0/' \
-	    -e 's/^ShowRobotsStats=.*/ShowRobotsStats=0/' \
-	    -e 's/^ShowOSStats=.*/ShowOSStats=0/' \
-	    -e 's/^ShowBrowsersStats=.*/ShowBrowsersStats=0/' \
-	    -e 's/^ShowOriginStats=.*/ShowOriginStats=0/' \
-	    -e 's/^ShowKeyphrasesStats=.*/ShowKeyphrasesStats=0/' \
-	    -e 's/^ShowKeywordsStats=.*/ShowKeywordsStats=0/' \
-	    -e 's/^ShowMiscStats=.*/ShowMiscStats=0/' \
-	    -e 's/^ShowHTTPErrorsStats=.*/ShowHTTPErrorsStats=0/' \
-	    < /etc/awstats/awstats.model.conf \
-	    > /etc/awstats/awstats.$HOTSPOT.conf
+	cp /etc/awstats/awstats.model.conf /etc/awstats/awstats.$HOTSPOT.conf
+	while read line; do
+		sed -i "s,^${line%=*}.*,$line," /etc/awstats/awstats.$HOTSPOT.conf
+	done <<EOT
+LogFile="$0 < /var/log/squid/access.log |"
+LogFormat="%code %time4 %bytesd %method %query %host %url"
+SiteDomain="$HOTSPOT"
+DirData="$DIRDATA"
+LevelForBrowsersDetection=0
+LevelForOSDetection=0
+LevelForRefererAnalyze=0
+LevelForRobotsDetection=0
+LevelForSearchEnginesDetection=0
+LevelForKeywordsDetection=0
+ShowRobotsStats=0
+ShowOSStats=0
+ShowBrowsersStats=0
+ShowOriginStats=0
+ShowKeyphrasesStats=0
+ShowKeywordsStats=0
+ShowMiscStats=0
+ShowHTTPErrorsStats=0
+EOT
 else
 	while read time skip1 iprouter skip2 bytesd method query skip3 ; do
 		host=$(echo $query | sed 's#.*//\([^/]*\)/.*#\1#')
-		#url=$(echo $query | sed 's#.*//[^/]*##')
 		url=$(echo $query | sed 's#.*//##')
 		echo "200 ${time%.*} $bytesd $method $query $host $url"
 	done
